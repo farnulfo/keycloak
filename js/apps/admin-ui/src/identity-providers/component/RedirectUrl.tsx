702f961da05d89702ff1ebaa7d2361f9003db27d
Franck Arnulfo <franck.arnulfo@gmail.com>
2026-06-13 09:26:08 +0000

    Warn when broker redirect URI may differ from realm frontend URL

--- js/apps/admin-ui/src/identity-providers/component/RedirectUrl.tsx
+++ js/apps/admin-ui/src/identity-providers/component/RedirectUrl.tsx
@@ -1,4 +1,10 @@
-import { ClipboardCopy, FormGroup } from "@patternfly/react-core";
+import {
+  ClipboardCopy,
+  FormGroup,
+  FormHelperText,
+  HelperText,
+  HelperTextItem,
+} from "@patternfly/react-core";
 import { useTranslation } from "react-i18next";
 import { HelpItem, useEnvironment } from "@keycloak/keycloak-ui-shared";
 import { useRealm } from "../../context/realm-context/RealmContext";
@@ -8,10 +14,18 @@ export const RedirectUrl = ({ id }: { id: string }) => {
   const { environment } = useEnvironment();
   const { t } = useTranslation();
 
-  const { realm } = useRealm();
+  const { realm, realmRepresentation } = useRealm();
   const callbackUrl = `${addTrailingSlash(
     environment.serverBaseUrl,
   )}realms/${realm}/broker`;
+  const redirectUrl = `${callbackUrl}/${id}/endpoint`;
+  const frontendUrl = realmRepresentation.attributes?.frontendUrl;
+  const frontendRedirectUrl = frontendUrl
+    ? `${addTrailingSlash(frontendUrl)}realms/${realm}/broker/${id}/endpoint`
+    : undefined;
+  const showFrontendUrlWarning =
+    frontendRedirectUrl !== undefined &&
+    frontendRedirectUrl !== redirectUrl;
 
   return (
     <FormGroup
@@ -21,9 +35,18 @@ export const RedirectUrl = ({ id }: { id: string }) => {
       }
       fieldId="kc-redirect-uri"
     >
-      <ClipboardCopy
-        isReadOnly
-      >{`${callbackUrl}/${id}/endpoint`}</ClipboardCopy>
+      <ClipboardCopy isReadOnly>{redirectUrl}</ClipboardCopy>
+      {showFrontendUrlWarning && (
+        <FormHelperText>
+          <HelperText>
+            <HelperTextItem variant="warning">
+              {t("redirectURIFrontendUrlWarning", {
+                url: frontendRedirectUrl,
+              })}
+            </HelperTextItem>
+          </HelperText>
+        </FormHelperText>
+      )}
     </FormGroup>
   );
 };
